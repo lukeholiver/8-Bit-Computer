a4455ba40744c39d93cbcc1567c8b2bffeb8edb3
lukeholiver <ttr3te@virginia.edu>
2026-09-23 00:08:59 -0400

    small i/o testbench

--- io_tb.v
+++ io_tb.v
@@ -1,56 +1,39 @@
 module io_tb(
     input clk,
     input reset,
-    input rx,
-    input [2:0] sw,
 
-    output [6:0] seg,
+    output reg [6:0] seg,
     output [3:0] an
 );
-
-
-
-/*
-What do we want to test?
-- LEDs and switches - all 16
-- Count to F on 7 seg display
-- 
-
-
-*/
 
 // Counter on 7 seg display
 
 // first display
-assign an <= 4'b1110;
-
-reg [15:0] clk_counter;
+assign an = 4'b1110;
+reg [25:0] clk_counter;
 reg [3:0] nibble;
 wire [6:0] decoded_seg;
-
 
 always @(posedge clk) begin
 
         if(reset) begin
             clk_counter <= 0;
-            seg <= 7'b0111111; 
+            seg <= 7'b0111111;
+            nibble <= 0; 
         end
         else begin
             clk_counter <= clk_counter + 1;
-            if()
-
+            if(clk_counter == 26'd50000000) begin
+                nibble <= nibble + 1;
+                seg <= decoded_seg;
+            end
         end 
-
 end
 
+    // segment decoder module
         segment_decoder segment_decoder(
         .nibble     (nibble),
         .seg        (decoded_seg)
     );
 
-
-
 endmodule
-
-
-
